7c71aed834cd2859da8048ca8824aa26910c47bb
Raid <rjalquizar@gmail.com>
2021-04-21 17:20:22 +0800

    Files for calculator and task assigned to each

---
+++ cal.py
@@ -0,0 +1,35 @@
+import math
+
+a = int(input('Enter value of a: '))
+b = int(input('Enter value of b: '))
+
+#Addition
+
+#Subtraction
+
+#Multiplication
+
+#Division
+
+print("Enter your choice: ")
+print("a. Add")
+print("b. Subtract")
+print("c. Multiplication")
+print("d. Division")
+
+choice = input("Enter choice: ")
+
+if choice == 'a':
+    
+
+elif choice == 'b':
+
+
+elif choice == 'c':
+    
+
+elif choice == 'd':
+    
+	
+else:
+    print("Invalid choice")
